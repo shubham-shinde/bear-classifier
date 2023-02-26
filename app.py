128e8e3258d3a9d761dba2be0a946ad52e0018e6
kaizen97 <shinde.shubham@byjus.com>
2023-02-26 17:45:56 +0530

    performing app

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import gradio as gr
+from fastai.vision.all import *
+import torch
+
+learn = load_learner('model.pkl')
+categories = learn.dls.vocab
 
 def greet(name):
     return "Hello " + name + "!!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+def bear_classifier(image):
+    pred, idx, probs = learn.predict(image)
+    return dict(zip(categories, map(float, probs)))
+
+image = gr.Image(shape=(224, 224))
+label = gr.outputs.Label()
+examples = ['panda.jpeg', 'poler.jpeg']
+
+iface = gr.Interface(bear_classifier, inputs=image, outputs=label, examples=examples)
 iface.launch()
